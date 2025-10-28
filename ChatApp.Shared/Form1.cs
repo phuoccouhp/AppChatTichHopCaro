fc5637be70b9461bb41576ade5c7ea94f4f1918b
phuoccouhp <huyphuoc09112005@gmail.com>
2025-10-28 13:12:08 +0700

    xoa sua them tinh nagn
    xoa sua them tinh nagn

---
+++ ChatApp.Shared/Form1.cs
@@ -0,0 +1,10 @@
+namespace ChatApp.Shared
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ChatApp.Shared/obj/Debug/net8.0-windows/ChatApp.Shared.AssemblyInfo.cs
+++ ChatApp.Shared/obj/Debug/net8.0-windows/ChatApp.Shared.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ChatAppClient")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57d1b14655ec960a8fc080c4d7d18211aeee4d7")]
-[assembly: System.Reflection.AssemblyProductAttribute("ChatAppClient")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ChatAppClient")]
+[assembly: System.Reflection.AssemblyProductAttribute("ChatApp.Shared")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ChatApp.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
