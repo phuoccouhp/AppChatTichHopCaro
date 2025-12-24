2bf5d20cd50ea11f987d94697812901cf4fdebb4
Huy Phước <203403593+phuoccouhp@users.noreply.github.com>
2025-12-24 14:40:42 +0700

    ok

--- ChatApp.Shared/obj/Debug/net8.0-windows/ChatApp.Shared.AssemblyInfo.cs
+++ ChatApp.Shared/obj/Debug/net8.0-windows/ChatApp.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ca68f671cef5f380bb8e3763459263df1fce98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fbb4ede1859fdfbe59872196a1e49dc32c362c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
